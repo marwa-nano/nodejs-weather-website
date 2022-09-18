1052b0f02e2b1c9599f1dbb672fba9468a6514f3
marwa-nano <marwa.nanodegree@gmail.com>
2022-09-18 20:23:36 +0200

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 // define path for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
@@ -103,8 +104,8 @@ app.get("*", (req, res) => {
 });
 
 // start server
-app.listen(3000, () => {
-  console.log("server is up on port 3000");
+app.listen(port, () => {
+  console.log("server is up on port " + port);
 });
 
 /////////
